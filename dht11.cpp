a589216d48a239ef6fc101a1dbddc4ac51ec2994
Junhyung Park <junhyung1821@gmail.com>
2020-05-13 00:47:48 -0500

    DHT11 Initial

---
+++ dht11.cpp
@@ -0,0 +1,28 @@
+//include <SimpleDHT.h>
+
+// for DHT11, 
+//      VCC: 5V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {  
+  // read with raw sample data.
+  byte temperature = 0;
+  byte humidity = 0;
+  byte data[40] = {0};
+  if (dht11.read(pinDHT11, &temperature, &humidity, data)) {
+    return;
+  }
+  
+  Serial.print((int)temperature); Serial.print(" *C, ");
+  Serial.print((int)humidity); Serial.println(" %");
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(10000);
+}

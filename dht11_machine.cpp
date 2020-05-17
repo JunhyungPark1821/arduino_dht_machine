1b928c6ec17e487d141552f240ccf4c8b50be498
Junhyung Park <junhyung1821@gmail.com>
2020-05-17 02:39:25 -0500

    DHT11-LCD-Dehumidifier Connection 05172020
    -First successful trial with LCD displaying the data from DHT11 and the dehumidifier controlled by a relay and the humidity data from DHT11.
    *5V SRD-05VDC-SL-C Relay is getting overheated by a 12V DC power supply from the dehumidifier.  Will need to research more into this issue. Most likely be a hardware issue.

---
+++ dht11_machine.cpp
@@ -0,0 +1,57 @@
+#include <SimpleDHT.h>
+#include <LiquidCrystal.h>
+
+
+// for DHT11, 
+//      VCC: 3.5V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;  //Second pin receives the data from DHT11
+SimpleDHT11 dht11;  //DHT11 electrical component
+
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+void setup() {
+  //set pin direction
+  pinMode(4, OUTPUT);
+  Serial.begin(9600);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Temp: ");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity: ");
+  }
+
+void loop() {
+  //DHT11 read with raw sample data.
+  byte temperature = 0;
+  byte humidity = 0;
+  byte data[40] = {0};
+  if (dht11.read(pinDHT11, &temperature, &humidity, data)) {
+    return;
+  }
+
+  //Relay conditions
+  if ((int) humidity > 60) {  //If the humidity percentage is over 60%
+      digitalWrite (4,LOW); //Circuit is closed
+  }
+  else {
+    digitalWrite (4, HIGH); //Circuit is open
+  }
+  
+  //LCD Display of temperature and humidity
+  lcd.setCursor(6,0);
+  lcd.print ((int)temperature);
+  lcd.print (" 'C");
+  lcd.setCursor(10,1);
+  lcd.print ((int)humidity);
+  lcd.print (" %");
+  Serial.print((int)temperature); Serial.print(" 'C, ");
+  Serial.print((int)humidity); Serial.println(" %");
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(5000);
+}
